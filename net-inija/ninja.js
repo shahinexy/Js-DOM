8cfd5eb34ea867c0c2b4bc95be32b3ebb5eba774
shahinexy <shahinexy@gmail.com>
2023-12-10 23:26:43 +0600

    modify js file

--- net-inija/ninja.js
+++ net-inija/ninja.js
@@ -1,8 +1,19 @@
-const itemDelete = document.querySelectorAll('.container .delete');
-Array.from(itemDelete).forEach(function(btn){
-    btn.addEventListener('click', function(x){
-        const li = x.target.parentElement;
+// const itemDelete = document.querySelectorAll('.container .delete');
+// Array.from(itemDelete).forEach(function(btn){
+//     btn.addEventListener('click', function(x){
+//         const li = x.target.parentElement;
+//         li.parentElement.removeChild(li);
+//         // you can also use parentNode in the place of parentElement 
+//     })
+// })
+
+
+// the same work in dynamic way 
+// =================================
+const listUl = document.querySelector('.container ul');
+listUl.addEventListener('click', function(click){
+    if(click.target.className == 'delete'){
+        const li = click.target.parentElement;
         li.parentElement.removeChild(li);
-        // you can also use parentNode in the place of parentElement 
-    })
+    }
 })
