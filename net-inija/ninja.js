503e454f7595e1c0c13de35216efd2f163d3f3a8
shahinexy <shahinexy@gmail.com>
2023-12-12 23:28:34 +0600

    tabbed fild work

--- net-inija/ninja.js
+++ net-inija/ninja.js
@@ -73,3 +73,20 @@ searchList.addEventListener('keyup', function(e){
     })
 
 })
+
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelector('.panel');
+tabs.addEventListener('click', function(e){
+    if(e.target.tagName == 'LI'){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel){
+            if(panel == targetPanel){
+                panel.classList.add('active');
+            } else{
+                panel.classList.remove('active');
+            }
+        }) 
+    }
+})
